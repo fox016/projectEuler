fc868fbb195981231f8c7fcb55d3c6e843486629
Nathan Fox <fox016@gmail.com>
2014-03-05 22:32:13 -0700

    I finished 80 and I am awesome

--- euler80.py
+++ euler80.py
@@ -1,13 +1,4 @@
 from decimal import *
+getcontext().prec=105
 
-getcontext().prec=100
-
-"""
-total = 0
-for n in xrange(1, 100):
-	numStr = str(Decimal(n) ** Decimal('0.5'))[2:]
-	total += sum(map(int, list(numStr)))
-print total
-"""
-
-print sum(map(int, list(''.join([str(Decimal(n) ** Decimal('0.5'))[2:] for n in xrange(1, 100)]))))
+print sum(map(int, list(''.join([str(Decimal(n).sqrt()).replace('.', '')[:100] for n in xrange(1, 100) if n not in [j*j for j in xrange(10)]]))))
