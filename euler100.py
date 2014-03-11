f1f2d691bbf3271f2b19b90098eada6004db679b
Nathan Fox <fox016@gmail.com>
2014-03-11 12:23:31 -0600

    finished 100

--- euler100.py
+++ euler100.py
@@ -1,3 +1,5 @@
 blue, total = 15, 21
 while total < 10**12: blue, total = 3 * blue + 2 * total - 2, 4 * blue + 3 * total - 3
 print blue, total
+
+# see http://www.mathblog.dk/project-euler-100-blue-discs-two-blue/
