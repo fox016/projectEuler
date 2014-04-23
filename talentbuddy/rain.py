b7e61c252ab8c09f5ce9dae8ad35f9d960489530
Nathan Fox <fox016@gmail.com>
2014-04-23 12:56:30 -0600

    finished rain :)

--- talentbuddy/rain.py
+++ talentbuddy/rain.py
@@ -1,49 +1,69 @@
-"""
-def rain(m, heights):
-	grid = list()
-	for row in xrange(len(heights) / m):
-		gridRow = list()
-		for col in xrange(m):
-			gridRow.append(heights[row * m + col])
-		grid.append(gridRow)
-	for row in xrange(len(grid)):
-		if row == 0 or row == len(grid)-1:
-			continue
-		for col in xrange(len(grid[row])):
-			if col == 0 or col == len(grid[row])-1:
-				continue
-			print grid[row][col]
-"""
-
-"""
 def rain(m, heights):
 	vol = 0
-	rows = len(heights) / m
-	cols = m
 	dirty = True
-	print heights
 	while dirty:
 		dirty = False
 		for index in xrange(len(heights)):
-			row = index / m
-			col = index % m
 			height = heights[index]
-			if row == 0 or col == 0 or row == (rows-1) or col == (cols-1):
+			shortestIndex = get_smallest_neighbor(index, m, heights)
+			if shortestIndex == -1:
 				continue
-			neighbors = [index-m, index+m, index-1, index+1]
-			shortestIndex = min(neighbors, key = lambda i: heights[i])
-			while heights[shortestIndex] == height:
-				neighbors.remove(shortestIndex)
-				# TODO
-				break
 			if heights[shortestIndex] > height:
 				vol += (heights[shortestIndex] - height)
 				heights[index] += (heights[shortestIndex] - height)
 				dirty = True
-		print heights
+			if heights[shortestIndex] == height:
+				equal = get_equal_group(index, m, heights, set([index]))
+				sides = set()
+				for i in equal:
+					sides |= set(get_filtered_neighbors(i, m, heights, lambda x: (heights[x] != heights[index])))
+				if -1 in sides or not sides:
+					continue
+				smallest = min(sides, key = lambda n: heights[n])
+				if heights[smallest] > height:
+					for i in equal:
+						vol += (heights[smallest] - height)
+						heights[i] += (heights[smallest] - height)
+					dirty = True
 	print vol
-"""
 
-m = 6
-heights = [3, 3, 4, 4, 4, 2, 3, 1, 3, 2, 1, 4, 7, 3, 1, 6, 4, 1]
+def get_neighbors(index, m, heights):
+	neighbors = list()
+	if index >= len(heights):
+		return neighbors
+	row = index / m
+	col = index % m
+	if row > 0:
+		neighbors.append(index-m)
+	if row < (len(heights)/m)-1:
+		neighbors.append(index+m)
+	if col > 0:
+		neighbors.append(index-1)
+	if col < m-1:
+		neighbors.append(index+1)
+	while len(neighbors) < 4:
+		neighbors.append(-1)
+	return neighbors
+
+def get_smallest_neighbor(index, m, heights):
+	neighbors = get_neighbors(index, m, heights)
+	if -1 in neighbors:
+		return -1
+	return min(neighbors, key = lambda n: heights[n])
+
+def get_filtered_neighbors(index, m, heights, fltr):
+	return filter(fltr, get_neighbors(index, m, heights))
+
+def get_equal_group(index, m, heights, index_set):
+	equal = get_filtered_neighbors(index, m, heights, lambda x: (heights[x] == heights[index]) and (x not in index_set))
+	if len(equal) == 0:
+		return index_set
+	index_set |= set(equal)
+	return_set = set()
+	for i in equal:
+		return_set |= get_equal_group(i, m, heights, index_set)
+	return return_set
+
+m = 3
+heights = [5, 5, 5, 5, 1, 5, 5, 5, 5]
 rain(m, heights)
