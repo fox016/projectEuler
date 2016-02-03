f5ce45de88537959910e383caa0f3b0cc170301b
Nathan Fox <fox016@gmail.com>
2016-02-03 09:26:32 -0700

    condensed euler 84

--- euler84.py
+++ euler84.py
@@ -1,89 +1,69 @@
 import random
 
-squares = [
-	"GO", "A1", "CC1", "A2", "T1", "R1", "B1", "CH1", "B2", "B3",
-	"JAIL", "C1", "U1", "C2", "C3", "R2", "D1", "CC2", "D2", "D3",
-	"FP", "E1", "CH2", "E2", "E3", "R3", "F1", "F2", "U2", "F3",
-	"G2J", "G1", "G2", "CC3", "G3", "R4", "CH3", "H1", "T2", "H2"
-]
-dice = range(1, 5)
-position = 0
-rolls = 1000000
-square_count_map = {}
-for square in squares:
-	square_count_map[square] = 0
-doubles_count = 0
+cc_map = {0:0, 1:10}
+ch_map = {0:0, 1:10, 2:11, 3:24, 4:39, 5:5}
 
-def draw_cc(position, square):
+def draw_cc(position):
 	card_num = random.randint(0, 15)
-	if(card_num == 0):
-		return 0, "GO"
-	if(card_num == 1):
-		return 10, "JAIL"
-	return position, square
+	if card_num in cc_map:
+		return cc_map[card_num]
+	return position
 
-def draw_ch(position, square):
+def draw_ch(position):
 	card_num = random.randint(0, 15)
-	if(card_num == 0):
-		return 0, "GO"
-	if(card_num == 1):
-		return 10, "JAIL"
-	if(card_num == 2):
-		return 11, "C1"
-	if(card_num == 3):
-		return 24, "E3"
-	if(card_num == 4):
-		return 39, "H2"
-	if(card_num == 5):
-		return 5, "R1"
+	if card_num in ch_map:
+		return ch_map[card_num]
 	if(card_num == 6 or card_num == 7):
 		if position < 5:
-			return 5, "R1"
+			return 5
 		if position < 15:
-			return 15, "R2"
+			return 15
 		if position < 25:
-			return 25, "R3"
+			return 25
 		if position < 35:
-			return 35, "R4"
-		return 5, "R1"
+			return 35
+		return 5
 	if(card_num == 8):
 		if position < 12:
-			return 12, "U1"
+			return 12
 		if position < 28:
-			return 28, "U2"
-		return 12, "U1"
+			return 28
+		return 12
 	if(card_num == 9):
-		if position == 7:
-			return 4, "T1"
-		if position == 22:
-			return 19, "D3"
 		if position == 36:
-			return draw_cc(33, "CC3")
-	return position, square
+			return draw_cc(33)
+		return position-3
+	return position
+
+position = 0
+rolls = 100000
+square_count_map = {}
+for i in xrange(40):
+	square_count_map[i] = 0
+doubles_count = 0
 
 for i in xrange(rolls):
-	d1 = random.choice(dice)
-	d2 = random.choice(dice)
+	d1 = random.randint(1,4)
+	d2 = random.randint(1,4)
 	if(d1 == d2):
 		doubles_count += 1
 	else:
 		doubles_count = 0
 	if(doubles_count == 3):
-		square = "JAIL"
 		position = 10
 		doubles_count = 0
 	else:
 		position += (d1 + d2)
-		position %= len(squares)
-		square = squares[position]
-	if(square == "G2J"):
-		square = "JAIL"
+		position %= 40
+	if(position == 30):
 		position = 10
-	elif(square[:2] == "CC"):
-		position, square = draw_cc(position, square)
-	elif(square[:2] == "CH"):
-		position, square = draw_ch(position, square)
-	square_count_map[square]+=1
+	elif(position in [2,17,33]):
+		position = draw_cc(position)
+	elif(position in [7,22,36]):
+		position = draw_ch(position)
+	square_count_map[position]+=1
 
-for square in squares:
-	print square, square_count_map[square], round(square_count_map[square] / float(rolls), 4)
+for square in xrange(40):
+	print square, round(100 * square_count_map[square] / float(rolls), 4)
+
+print sorted(square_count_map, key=lambda square: square_count_map[square])[:-4:-1]
