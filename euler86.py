b642af52312c949845e105e8ffadd9acefdc0367
Nathan Fox <fox016@gmail.com>
2016-02-04 09:45:15 -0700

    finished 86

--- euler86.py
+++ euler86.py
@@ -1,50 +1,24 @@
 import math
 from fractions import gcd
 
-def count_integer_routes(M):
-	short_route_int_count = 0
-	for x in xrange(1, M+1):
-		for y in xrange(x, M+1):
-			for z in xrange(y, M+1):
-				a = z
-				b = x + y
-				c = math.sqrt(a**2 + b**2)
-				if c == round(c):
-					print (x,y,z), (a,b,c)
-					short_route_int_count += 1
-	return short_route_int_count
-
 def count_from_triples(M):
 	short_route_int_count = 0
 	triples = generate_all_triples(100, M*3)
 	for triple in triples:
-		short_route_int_count += count_options(triple)
+		short_route_int_count += count_abc_options(triple, M)
 	return short_route_int_count
 
-def count_options(triple):
+def count_abc_options(triple, M):
+	return count_xyz_options(1, triple[1]-1, triple[0], M) + count_xyz_options(1, triple[0]-1, triple[1], M)
+
+def count_xyz_options(x, y, z, M):
+	if z > M:
+		return 0
 	count = 0
-	z = triple[0]
-	x = 1
-	y = triple[1] - x
-	while y > z:
-		y -= 1
-		x += 1
-	while x <= y:
-		print x, y, z
-		count += 1
-		y -= 1
-		x += 1
-	z = triple[1]
-	x = 1
-	y = triple[0] - x
-	while y > z:
-		y -= 1
-		x += 1
-	while x <= y:
-		print x, y, z
-		count += 1
-		y -= 1
-		x += 1
+	if y > z:
+		x, y = x+y-z, z
+	if x <= y:
+		count += ((y-x)/2) + 1
 	return count
 
 def generate_primitive_triples(iter_count, max_c):
@@ -68,7 +42,5 @@ def generate_all_triples(iter_count, max_c):
 			k+=1
 	return triples
 
-M = 100
-#print count_integer_routes(M)
-#print count_from_triples(M)
-print count_options((6,8,10)) # Should be 6
+M = 1818
+print count_from_triples(M)
