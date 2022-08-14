1f27b64ea787bf25862e263e6525b936e7c06f62
Nathan Fox <fox016@gmail.com>
2022-08-14 13:22:20 -0600

    finished 751

--- euler751.py
+++ euler751.py
@@ -8,14 +8,24 @@ def get_a(theta, n=24):
         prev = Decimal(b[i-1])
         f = int(floor(prev))
         b[i] = Decimal(f * (prev - f + 1))
-    return map(floor, b)
+    return map(int, map(floor, b))
 
-theta = Decimal(2.223561019313554106173177)
-a = get_a(theta, 24)
-tau = "".join(map(str, a))
-tau_f = Decimal(tau[0] + "." + tau[1:])
-print(tau)
-print(tau_f)
+place = Decimal(1.0/10.0)
+theta = Decimal(2.0)
+for i in range(24):
+    next_digit = None
+    for d in range(10):
+        theta_tmp = theta + (place * d)
+        a = get_a(theta_tmp, 24)
+        tau = "".join(map(str, a))
+        tau_f = Decimal(tau[0] + "." + tau[1:])
+        diff = tau_f-theta_tmp
+        if diff < 0:
+            next_digit = d-1
+            break
+    if next_digit == None:
+        next_digit = 9
+    theta = theta + (place * next_digit)
+    place=place/Decimal(10.0)
+
 print(theta)
-print(abs(tau_f-theta))
-print("")
